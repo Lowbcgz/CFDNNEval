b9843ff70098df8bfd451a06bd67548886ea56a8
CenJH <53068296+12138xs@users.noreply.github.com>
2024-05-21 12:20:33 +0800

    Add cylinder dataloader

--- dataset/__init__.py
+++ dataset/__init__.py
@@ -3,3 +3,5 @@ from .Darcy import DarcyDataset, PDEDarcyDataset
 from .NSCH import NSCHDataset
 from .tube import TubeDataset
 from .TGV import TGVDataset
+from .cylinder import CylinderDataset
+from .ircylinder import IRCylinderDataset
