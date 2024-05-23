b2a783354253a969e56cb03906be9abb6dd864c5
lihongji <1311109089@qq.com>
2024-05-23 16:24:36 +0800

    modify scripts

--- scripts/test_KNO.sh
+++ scripts/test_KNO.sh
@@ -1,31 +1,35 @@
 # tube
-CUDA_VISIBLE_DEVICES=0 python train.py ./config/KNO/config_tube_prop_bc_geo.yaml -c geo --test | tee ./log/test/KNO_tube_geo.out
-CUDA_VISIBLE_DEVICES=0 python train.py ./config/KNO/config_tube_prop_bc_geo.yaml -c bc --test | tee ./log/test/KNO_tube_bc.out
-CUDA_VISIBLE_DEVICES=0 python train.py ./config/KNO/config_tube_prop_bc_geo.yaml -c prop --test | tee ./log/test/KNO_tube_prop.out
-CUDA_VISIBLE_DEVICES=0 python train.py ./config/KNO/config_tube_prop_bc_geo.yaml -c bc_geo --test | tee ./log/test/KNO_tube_bc_geo.out
-CUDA_VISIBLE_DEVICES=0 python train.py ./config/KNO/config_tube_prop_bc_geo.yaml -c prop_geo --test | tee ./log/test/KNO_tube_prop_geo.out
-CUDA_VISIBLE_DEVICES=0 python train.py ./config/KNO/config_tube_prop_bc_geo.yaml -c prop_bc --test | tee ./log/test/KNO_tube_prop_bc.out
-CUDA_VISIBLE_DEVICES=0 python train.py ./config/KNO/config_tube_prop_bc_geo.yaml -c prop_bc_geo --test | tee ./log/test/KNO_tube_prop_bc_geo.out
+CUDA_VISIBLE_DEVICES=0 python train.py ./config/KNO/config_tube_prop_bc_geo.yaml -c geo --test | tee ./log/KNO/test/KNO_tube_geo.out
+CUDA_VISIBLE_DEVICES=0 python train.py ./config/KNO/config_tube_prop_bc_geo.yaml -c bc --test | tee ./log/KNO/test/KNO_tube_bc.out
+CUDA_VISIBLE_DEVICES=0 python train.py ./config/KNO/config_tube_prop_bc_geo.yaml -c prop --test | tee ./log/KNO/test/KNO_tube_prop.out
+CUDA_VISIBLE_DEVICES=0 python train.py ./config/KNO/config_tube_prop_bc_geo.yaml -c bc_geo --test | tee ./log/KNO/test/KNO_tube_bc_geo.out
+CUDA_VISIBLE_DEVICES=0 python train.py ./config/KNO/config_tube_prop_bc_geo.yaml -c prop_geo --test | tee ./log/KNO/test/KNO_tube_prop_geo.out
+CUDA_VISIBLE_DEVICES=0 python train.py ./config/KNO/config_tube_prop_bc_geo.yaml -c prop_bc --test | tee ./log/KNO/test/KNO_tube_prop_bc.out
+CUDA_VISIBLE_DEVICES=0 python train.py ./config/KNO/config_tube_prop_bc_geo.yaml -c prop_bc_geo --test | tee ./log/KNO/test/KNO_tube_prop_bc_geo.out
 
 # TGV
-CUDA_VISIBLE_DEVICES=0 python train.py ./config/KNO/config_TGV_all.yaml -c all --test | tee ./log/test/KNO_tgv_all.out
-CUDA_VISIBLE_DEVICES=0 python train.py ./config/KNO/config_TGV_all.yaml -c single --test | tee ./log/test/KNO_single.out
-
+CUDA_VISIBLE_DEVICES=0 python train.py ./config/KNO/config_TGV_all.yaml -c all --test | tee ./log/KNO/test/KNO_tgv_all.out
+CUDA_VISIBLE_DEVICES=0 python train.py ./config/KNO/config_TGV_all.yaml -c single --test | tee ./log/KNO/test/KNO_tgv_single.out
 
 # cavity
-CUDA_VISIBLE_DEVICES=0 python train.py ./config/KNO/config_cavity_ReD_bc_re.yaml -c ReD --test | tee ./log/test/KNO_cavity_ReD.out
-CUDA_VISIBLE_DEVICES=0 python train.py ./config/KNO/config_cavity_ReD_bc_re.yaml -c bc --test | tee ./log/test/KNO_cavity_bc.out
-CUDA_VISIBLE_DEVICES=0 python train.py ./config/KNO/config_cavity_ReD_bc_re.yaml -c re --test | tee ./log/test/KNO_cavity_re.out
-CUDA_VISIBLE_DEVICES=0 python train.py ./config/KNO/config_cavity_ReD_bc_re.yaml -c ReD_bc_re --test | tee ./log/test/KNO_cavity_ReD_bc_re.out
+CUDA_VISIBLE_DEVICES=0 python train.py ./config/KNO/config_cavity_ReD_bc_re.yaml -c ReD --test | tee ./log/KNO/test/KNO_cavity_ReD.out
+CUDA_VISIBLE_DEVICES=0 python train.py ./config/KNO/config_cavity_ReD_bc_re.yaml -c bc --test | tee ./log/KNO/test/KNO_cavity_bc.out
+CUDA_VISIBLE_DEVICES=0 python train.py ./config/KNO/config_cavity_ReD_bc_re.yaml -c re --test | tee ./log/KNO/test/KNO_cavity_re.out
+CUDA_VISIBLE_DEVICES=0 python train.py ./config/KNO/config_cavity_ReD_bc_re.yaml -c ReD_bc_re --test | tee ./log/KNO/test/KNO_cavity_ReD_bc_re.out
 
 # NSCH
-CUDA_VISIBLE_DEVICES=0 python train.py ./config/KNO/config_NSCH_ALL.yaml -c bc --test | tee ./log/test/KNO_nsch_bc.out
-CUDA_VISIBLE_DEVICES=0 python train.py ./config/KNO/config_NSCH_ALL.yaml -c ca --test | tee ./log/test/KNO_nsch_ca.out
-CUDA_VISIBLE_DEVICES=0 python train.py ./config/KNO/config_NSCH_ALL.yaml -c mob --test | tee ./log/test/KNO_nsch_mob.out
-CUDA_VISIBLE_DEVICES=0 python train.py ./config/KNO/config_NSCH_ALL.yaml -c phi --test | tee ./log/test/KNO_nsch_phi.out
-CUDA_VISIBLE_DEVICES=0 python train.py ./config/KNO/config_NSCH_ALL.yaml -c pre --test | tee ./log/test/KNO_nsch_pre.out
-CUDA_VISIBLE_DEVICES=0 python train.py ./config/KNO/config_NSCH_ALL.yaml -c re --test | tee ./log/test/KNO_nsch_re.out
-CUDA_VISIBLE_DEVICES=0 python train.py ./config/KNO/config_NSCH_ALL.yaml -c uv0 --test | tee ./log/test/KNO_nsch_uv0.out
-CUDA_VISIBLE_DEVICES=0 python train.py ./config/KNO/config_NSCH_ALL.yaml -c bc_ca_mob_phi_pre_re_uv0 --test | tee ./log/test/KNO_nsch_bc_ca_mob_phi_pre_re_uv0.out
-CUDA_VISIBLE_DEVICES=0 python train.py ./config/KNO/config_NSCH_ALL.yaml -c ca_mob_re --test | tee ./log/test/KNO_nsch_ca_mob_re.out
-CUDA_VISIBLE_DEVICES=0 python train.py ./config/KNO/config_NSCH_ALL.yaml -c phi_pre_uv0 --test | tee ./log/test/KNO_nsch_phi_pre_uv0.out
+CUDA_VISIBLE_DEVICES=0 python train.py ./config/KNO/config_NSCH_ALL.yaml -c bc --test | tee ./log/KNO/test/KNO_nsch_bc.out
+CUDA_VISIBLE_DEVICES=0 python train.py ./config/KNO/config_NSCH_ALL.yaml -c ca --test | tee ./log/KNO/test/KNO_nsch_ca.out
+CUDA_VISIBLE_DEVICES=0 python train.py ./config/KNO/config_NSCH_ALL.yaml -c mob --test | tee ./log/KNO/test/KNO_nsch_mob.out
+CUDA_VISIBLE_DEVICES=0 python train.py ./config/KNO/config_NSCH_ALL.yaml -c phi --test | tee ./log/KNO/test/KNO_nsch_phi.out
+CUDA_VISIBLE_DEVICES=0 python train.py ./config/KNO/config_NSCH_ALL.yaml -c pre --test | tee ./log/KNO/test/KNO_nsch_pre.out
+CUDA_VISIBLE_DEVICES=0 python train.py ./config/KNO/config_NSCH_ALL.yaml -c re --test | tee ./log/KNO/test/KNO_nsch_re.out
+CUDA_VISIBLE_DEVICES=0 python train.py ./config/KNO/config_NSCH_ALL.yaml -c uv0 --test | tee ./log/KNO/test/KNO_nsch_uv0.out
+CUDA_VISIBLE_DEVICES=0 python train.py ./config/KNO/config_NSCH_ALL.yaml -c bc_ca_mob_phi_pre_re_uv0 --test | tee ./log/KNO/test/KNO_nsch_bc_ca_mob_phi_pre_re_uv0.out
+CUDA_VISIBLE_DEVICES=0 python train.py ./config/KNO/config_NSCH_ALL.yaml -c ca_mob_re --test | tee ./log/KNO/test/KNO_nsch_ca_mob_re.out
+CUDA_VISIBLE_DEVICES=0 python train.py ./config/KNO/config_NSCH_ALL.yaml -c phi_pre_uv0 --test | tee ./log/KNO/test/KNO_nsch_phi_pre_uv0.out
+
+# cylinder
+CUDA_VISIBLE_DEVICES=3 python train.py ./config/KNO/config_cylinder_rRE_rBC.yaml -c rRE_rBC --test | tee ./log/KNO/test/KNO_cylinder_rRE_rBC.out
+CUDA_VISIBLE_DEVICES=3 python train.py ./config/KNO/config_cylinder_rRE_rBC.yaml -c rRE --test | tee ./log/KNO/test/KNO_cylinder_rRE.out
+CUDA_VISIBLE_DEVICES=3 python train.py ./config/KNO/config_cylinder_rRE_rBC.yaml -c rBC --test | tee ./log/KNO/test/KNO_cylinder_rBC.out
