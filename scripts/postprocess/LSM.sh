30565fc341c9898db31eb9dac0dc60b218fdc804
fhl2000 <2410591650@qq.com>
2024-06-02 00:23:37 +0800

    refine transfering script

--- scripts/postprocess/LSM.sh
+++ scripts/postprocess/LSM.sh
@@ -4,9 +4,6 @@ CUDA_VISIBLE_DEVICES=0 python extrapolation_test.py ./config/LSM/config_ircylind
 
 ### transfering
 # tube
-CUDA_VISIBLE_DEVICES=0 python transfering_test.py ./config/LSM/config_tube_prop_bc_geo.yaml -cs bc_geo -ct bc,geo,prop
-CUDA_VISIBLE_DEVICES=0 python transfering_test.py ./config/LSM/config_tube_prop_bc_geo.yaml -cs prop_geo -ct bc,geo,prop
-CUDA_VISIBLE_DEVICES=0 python transfering_test.py ./config/LSM/config_tube_prop_bc_geo.yaml -cs prop_bc -ct bc,geo,prop
 CUDA_VISIBLE_DEVICES=0 python transfering_test.py ./config/LSM/config_tube_prop_bc_geo.yaml -cs prop_bc_geo -ct bc,geo,prop
 
 # TGV
@@ -17,8 +14,8 @@ CUDA_VISIBLE_DEVICES=0 python transfering_test.py ./config/LSM/config_cavity_ReD
 
 # NSCH
 CUDA_VISIBLE_DEVICES=0 python transfering_test.py ./config/LSM/config_NSCH_ALL.yaml -cs ibc_phi_ca_mob_re_eps -ct ibc,phi,ca,mob,re,eps
-CUDA_VISIBLE_DEVICES=0 python transfering_test.py ./config/LSM/config_NSCH_ALL.yaml -cs ca_mob_re_eps -ct ibc,phi,ca,mob,re,eps
-CUDA_VISIBLE_DEVICES=0 python transfering_test.py ./config/LSM/config_NSCH_ALL.yaml -cs phi_ibc -ct ibc,phi,ca,mob,re,eps
+CUDA_VISIBLE_DEVICES=0 python transfering_test.py ./config/LSM/config_NSCH_ALL.yaml -cs ca_mob_re_eps -ct ca,mob,re,eps
+CUDA_VISIBLE_DEVICES=0 python transfering_test.py ./config/LSM/config_NSCH_ALL.yaml -cs phi_ibc -ct ibc,phi
 
 # Cylinder
 CUDA_VISIBLE_DEVICES=0 python transfering_test.py ./config/LSM/config_cylinder_rRE_rBC.yaml -cs rRE_rBC -ct rRE,rBC
